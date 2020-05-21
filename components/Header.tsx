d92fa7c67199372904a751259adfda9a705741cd
Brandon Day <brandontday7@gmail.com>
2020-05-20 20:50:47 -0400

    minor changes and styling for ipad

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,6 @@ import styled from "styled-components"
 
 const LOGO_WIDTH = 1925
 const LOGO_HEIGHT = 699
-const IMAGE_SIZE_RATIO = 0.75
 
 const WEBSITE_URL = "https://falconfinancialmodelling.com/"
 
@@ -13,10 +12,10 @@ const StyledImage = styled(Image)<{width: string, height: string}>`
   height: ${({ height }) => height};
 `
 
-const Background = styled(View)`
+const Background = styled(View)<{ipad?: boolean}>`
   flex: 1;
   background-color: rgb(51, 56, 62);
-  height: 190px;
+  height: ${({ ipad }) => ipad ? '220px' : '190px'};
   display: flex;
   justify-content: center;
   align-items: center;
@@ -25,11 +24,13 @@ const Background = styled(View)`
 
 const Header: FC = () => {
   const win = Dimensions.get('window')
-  const ratio = win.width / LOGO_WIDTH
-  const width = `${win.width  * IMAGE_SIZE_RATIO}px`
+  const windowWidth = win.width
+  const IMAGE_SIZE_RATIO = windowWidth > 1000 ? 0.5 : 0.75
+  const ratio = windowWidth / LOGO_WIDTH
+  const width = `${windowWidth * IMAGE_SIZE_RATIO}px`
   const height = `${LOGO_HEIGHT * ratio * IMAGE_SIZE_RATIO}px`
   return(
-    <Background>
+    <Background ipad={windowWidth > 1000}>
       <TouchableOpacity onPress={() => Linking.openURL(WEBSITE_URL)}>
         <StyledImage source={require('../images/logoFinal.png')} width={width} height={height} />
       </TouchableOpacity>
